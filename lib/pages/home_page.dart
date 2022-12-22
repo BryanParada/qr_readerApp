55720bc5d4c2c88938f5f1b7807ec07f4ecf39e5
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-22 17:57:40 -0300

    F163 - Provider as state manager

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/directions_page.dart';
 import 'package:qr_reader/pages/maps_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -35,8 +37,11 @@ class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    //obtener el seleceted menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
+
     //cambiar pagina respectiva
-    final currentIndex = 0;
+    final currentIndex = uiProvider.selectedMenuOpt;
 
     switch( currentIndex ){
 
