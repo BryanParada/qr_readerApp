a387a9a7789a2ea9cb9422c78b84cea6e95136fe
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-22 17:35:39 -0300

    F162 - Change page according to selected tab

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/pages/directions_page.dart';
+import 'package:qr_reader/pages/maps_page.dart';
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
@@ -18,12 +20,36 @@ class HomePage extends StatelessWidget {
              )
         ]
       ),
-      body: Center(
-         child: Text('HomePage'),
-      ),
+      body: _HomePageBody(),
+
       bottomNavigationBar: CustomNavigationBar(),
       floatingActionButton: ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+
+class _HomePageBody extends StatelessWidget { 
+
+  @override
+  Widget build(BuildContext context) {
+
+    //cambiar pagina respectiva
+    final currentIndex = 0;
+
+    switch( currentIndex ){
+
+      case 0:
+        return MapsPage();
+      
+      case 1:
+        return DirectionsPage();
+        
+      default:
+        return MapsPage();
+    }
+
+
   }
 }
