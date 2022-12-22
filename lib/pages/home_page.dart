ff1956f07d7e074c921d72ebc7662fa8cf945276
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-22 16:43:14 -0300

    F160 - app design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
 
 class HomePage extends StatelessWidget {
    
@@ -6,10 +8,22 @@ class HomePage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('History'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.delete_forever)
+             )
+        ]
+      ),
       body: Center(
          child: Text('HomePage'),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
