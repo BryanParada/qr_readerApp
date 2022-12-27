9220768539cb37ad830222a752424bd95f8fd452
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-26 21:51:36 -0300

    F181 - Optimize screens maps-directions

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/widgets/scan_tiles.dart';
 
 class MapsPage extends StatelessWidget {
   const MapsPage({super.key});
@@ -8,35 +9,7 @@ class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final scanListProvider = Provider.of<ScanListProvider>(context);//no se coloca listen:false, necesitamos que si se redibuje
-    final scans = scanListProvider.scans;
-    
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder:( _ , i) => Dismissible(
-        key: UniqueKey(),
-        direction: DismissDirection.endToStart, // swipe solo hacia la derecha 
-        background: Container(
-          color: Colors.red 
-        ),
-        onDismissed: (DismissDirection direction){
-          Provider.of<ScanListProvider>(context, listen: false).deleteScanById(scans[i].id!);
-        },
-        secondaryBackground: Container(
-        child: Icon(Icons.delete, color: Colors.white),
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.only(right: 20),
-        color: Colors.red,
-       ),
-        child: ListTile(
-          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-          title: Text(scans[i].value),
-          subtitle: Text( scans[i].id.toString()),
-          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          onTap: () => print(scans[i].id),
-          
-        ),
-      ),
-      );
+  return ScanTiles(type: 'http');
+
   }
 }
---
+++ lib/widgets/scan_tiles.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/scan_list_provider.dart';
+
+
+class ScanTiles extends StatelessWidget { 
+
+  final String type;
+
+  const ScanTiles({
+   super.key,
+   required this.type});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final scanListProvider = Provider.of<ScanListProvider>(context);//no se coloca listen:false, necesitamos que si se redibuje
+    final scans = scanListProvider.scans;
+    
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder:( _ , i) => Dismissible(
+        key: UniqueKey(),
+        direction: DismissDirection.endToStart, // swipe solo hacia la derecha 
+        background: Container(
+          color: Colors.red 
+        ),
+        onDismissed: (DismissDirection direction){
+          Provider.of<ScanListProvider>(context, listen: false).deleteScanById(scans[i].id!);
+        },
+        secondaryBackground: Container(
+        child: Icon(Icons.delete, color: Colors.white),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        color: Colors.red,
+       ),
+        child: ListTile(
+          leading: Icon(
+            this.type == 'http'
+            ? Icons.home_outlined
+            : Icons.map 
+            , color: Theme.of(context).primaryColor),
+          title: Text(scans[i].value),
+          subtitle: Text( scans[i].id.toString()),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print(scans[i].id),
+          
+        ),
+      ),
+      );
+
+
+    
+
+
+
+  }
+}
